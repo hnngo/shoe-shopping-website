2b89b931a2e269e204d63d7f667d531df13876bb
Nhan <hnngo@ntu.edu.sg>
2019-03-05 23:24:22 +0800

    Reload the react libraries

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
