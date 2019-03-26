34d7d13e7dc80ce6297800371f5f1945d64cebdf
Nhan <hnngo@ntu.edu.sg>
2019-03-26 23:47:10 +0800

    Feedback render random comments and names

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
     return (
       <div className="mt-4 pt-3">
         <ContactForm />
-        <div className="footer-container bg-secondary">
+        <div className="footer-container">
           <div className="container d-flex justify-content-between">
             <p className="mb-0 py-5 text-white">@ Copyright Shoeniverse | Terms | Policies</p>
             <p className="mb-0 py-5 text-white">2019</p>
--- src/components/landing_page/LandingFeedback.js
+++ src/components/landing_page/LandingFeedback.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
+const feedbacks = [
+  "Simply the best experience I have ever had buying running shoes and have bought a fair few over the years. Will definitely be buying shoes again from there and recommending to my friends.",
+  "We were very happy with the service provided by Shoeniverse. My sons are quite happy with their new shoes. Shoeniverse's staff was very pleasant, up to speed on the products, and generally looked after us well. We'll be back...",
+  "Just a quick note to say Shoeniverse was a pleasure to deal with. Great customer service and knowledge. We will be back this weekend come to purchase another pair of shoes.",
+  "One of the best shoe store ever, this is the third time I have been here and I will totally recommend my family and friends this store",
+  "I have ordered several pairs of sneakers from Shoeniverse. They have a relatively quick delivery. The prices are absolutely competitive!",
+  "Absolutely delighted with my new boots I brought from Shoeniverse! Great customer service skills had a query and they dealt with it so quickly and efficiently will highly recommend them to friends and family.",
+  "My first order with Shoe Zone was dealt with in a very good and helpful way from start to finish. The quality of the product is good and I'm very happy with the service and prompt delivery."
+];
+
+const name = [
+  "Lauren Macias",
+  "Sophie-Louise Sheppard",
+  "Natalia Castillo",
+  "Kalvin Milne",
+  "Sanjay Burnett",
+  "Tyreke Wharton",
+  "Aniela Robles"
+];
+
 export default () => {
   const mouseEnter = (e) => {
     // e.target.style.transform = "scale(1.25)";
@@ -50,7 +70,7 @@ export default () => {
       res.push(
         <div key={x} className="col-sm-1 my-auto">
           <img
-            src={`https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 100)}.jpg`}
+            src={`https://randomuser.me/api/portraits/women/${Math.floor(Math.random() * 100)}.jpg`}
             className="feedback-avatar img-ava"
             alt=""
             style={{ height: "100px" }}
@@ -69,10 +89,10 @@ export default () => {
     const res = [];
     for (let x = 1; x <= number; x++) {
       res.push(
-        <div key={x} className="col d-none animated fast" id={"cmt-" + x}>
-          <p className="monteserrat italic">Simply the best experience I have ever had buying running shoes and have bought a fair few over the years. Will definitely be buying shoes again from there and recommending to my friends.</p>
+        <div key={x} className="col px-3 d-none animated fast" id={"cmt-" + x}>
+          <p className="monteserrat italic">{feedbacks[x - 1]}</p>
           <p className="blockquote-footer text-white">
-            Ray Meade
+            {name[x - 1]}
           </p>
         </div>
       )
@@ -133,6 +153,3 @@ export default () => {
     </div>
   );
 }
-
-//TODO: Adding feedback background pill rectangle
-//TODO: Adding hover effect for feedback content or avatar
